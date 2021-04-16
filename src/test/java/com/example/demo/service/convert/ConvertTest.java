4e19707c032764ef9a6fed64438ac66cf4ebfb94
Victor Lins <victor.lins@orubetecnologia.com.br>
2021-04-16 13:08:07 -0300

    Corrigindo testes

--- src/test/java/com/example/demo/service/convert/ConvertTest.java
+++ src/test/java/com/example/demo/service/convert/ConvertTest.java
@@ -3,35 +3,35 @@ package com.example.demo.service.convert;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.justa.ConvertApplication;
 import com.justa.service.convert.Convert;
 
-
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = ConvertApplication.class)
 public class ConvertTest {
     
-    @SpyBean
-    Convert service;
+    @Autowired
+    private Convert service;
     
     @Test
-    private String getUnitNameTest() {
+    public void getUnitNameTest() {
 	String expression = "(degree/(minute*hectare))";
 	String convertExpression = service.getUnitName(expression);
 	
 	assertEquals(convertExpression, "(rad/(s*m²))");
-	return expression;
     }
     
     @Test
-    private String getMultiplicationFactorTest() {
+    public void getMultiplicationFactorTest() {
 	String expression = "(degree/(minute*hectare))";
 	String convertExpression = service.getMultiplicationFactor(expression);
 	
 	assertEquals(convertExpression, "0,000000029088820866572");
-	return expression;
     }
 
 }
